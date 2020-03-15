6dc16313276580ca0928aca140f8e3ba44c08934
lanhe <1095071913@qq.com>
2020-03-15 17:57:12 +0800

    作者:彭晋龙 时间:2020/03/15 说明:添加android模块,过几天后开始

---
+++ mountain-shop-parent/mountain-shop-client/mountain-shop-client-web/mountain-shop-client-web-admin/src/store/modules/user.js
@@ -0,0 +1,88 @@
+import { login, logout, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const state = {
+  token: getToken(),
+  name: '',
+  avatar: ''
+}
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ username: username.trim(), password: password }).then(response => {
+        const { data } = response
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(response => {
+        const data = response
+        if (data.code !== 200) {
+          reject('系统异常')
+        }
+        const { username, icon } = data.databeans[0].data
+
+        commit('SET_NAME', username)
+        commit('SET_AVATAR', icon)
+        resolve(response)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token).then(() => {
+        commit('SET_TOKEN', '')
+        removeToken()
+        resetRouter()
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      commit('SET_TOKEN', '')
+      removeToken()
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
---
+++ mountain-shop-parent/mountain-shop-client/mountain-shop-client-web/mountain-shop-client-web-admin/src/views/user/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="app-container">
+    <el-form
+      ref="form"
+      v-loading="formLoading"
+      :data="form"
+      element-loading-text="加载中..."
+      :model="form"
+      label-width="120px"
+    >
+      <el-form-item label="头像">
+        <img :src="form.icon" width="60" height="60">
+      </el-form-item>
+      <el-form-item label="账号">
+        <el-input v-model="form.username" :disabled="true" />
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <el-input v-model="form.email" />
+      </el-form-item>
+      <el-form-item label="昵称">
+        <el-input v-model="form.nickName" />
+      </el-form-item>
+      <el-form-item label="备注">
+        <el-input v-model="form.note" />
+      </el-form-item>
+      <el-form-item label="创建时间">
+        <el-input v-model="form.createTime" :disabled="true" />
+      </el-form-item>
+      <el-form-item label="最后登录">
+        <el-input v-model="form.loginTime" :disabled="true" />
+      </el-form-item>
+      <el-form-item label="是否启用">
+        <el-radio-group v-model="form.status">
+          <el-radio :label="0">禁用</el-radio>
+          <el-radio :label="1">启用</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+import { info } from '@/api/profile'
+export default {
+  name: 'ProfileInfo',
+  data() {
+    return {
+      formLoading: true,
+      form: {
+        icon: '',
+        username: '',
+        email: '',
+        nickName: '',
+        note: '',
+        createTime: '',
+        loginTime: '',
+        status: ''
+      }
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      info(this.$store.getters.name).then(response => {
+        this.form = response.data
+        this.formLoading = false
+      })
+    },
+    onSubmit() {
+      this.$message('submit!')
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
