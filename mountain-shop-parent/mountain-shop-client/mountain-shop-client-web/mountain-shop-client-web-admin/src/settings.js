b2021e34b21d71e108a0ff707e2f8b1561af36d4
lanhe <1095071913@qq.com>
2020-03-15 17:57:12 +0800

    作者:彭晋龙 时间:2020/03/15 说明:添加android模块,过几天后开始..

---
+++ mountain-shop-parent/mountain-shop-client/mountain-shop-client-web/mountain-shop-client-web-admin/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'mountain-shop-web-admin',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
